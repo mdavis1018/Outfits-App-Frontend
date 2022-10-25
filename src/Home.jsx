6ce513ea8b83afd07975e10bdea954df2cba0300
Marcus Davis <marcusdavis1018@gmail.com>
2022-10-25 12:41:15 -0400

    Index action using axios and useState/useEffect

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,11 +2,19 @@ import axios from "axios";
 import { OutfitsIndex } from "./OutfitsIndex";
 import { useState, useEffect } from "react";
 export function Home() {
-  // const [outfits, setOutfits] = useState([]);
-  const outfits = [
-    { id: 1, title: "test1" },
-    { id: 2, title: "test2" },
-  ];
+  const [outfits, setOutfits] = useState([]);
+  // const outfits = [
+  //   { id: 1, title: "test1" },
+  //   { id: 2, title: "test2" },
+  // ];
+  const handleIndexOutfits = () => {
+    console.log(handleIndexOutfits);
+    axios.get("http://localhost:3000/outfits.json").then((response) => {
+      console.log(response.data);
+      setOutfits(response.data);
+    });
+  };
+  useEffect(handleIndexOutfits, []);
   return (
     <div>
       <h1>Welcome to React!</h1>
